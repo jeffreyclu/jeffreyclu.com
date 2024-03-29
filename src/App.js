169ea88944273663c3b44507f2f3411f527c4db2
Jeffrey Lu <hi@jeffreyclu.com>
2024-03-29 14:17:03 -0400

    update links; add new projecs

--- src/App.js
+++ src/App.js
@@ -6,31 +6,44 @@ import './App.css';
 
 const data = [
   {
+    title: "Thememaker Chrome Extension",
+    technologies: ["HTML", "CSS", "JavaScript", "Chrome Extension", "The Color API"],
+    description: "A Chrome extension that lets users generate their own themes for any website, using the Color API. Color schemes are randomly generated, and can be persisted once applied. Users can also view up to 10 previously generated schemes in the same session.",
+    demoLink: "https://chromewebstore.google.com/detail/hlgomofojbinomfoilafddhbdapciodl",
+    sourceLink: "https://github.com/jeffreyclu/thememaker"
+  },
+  {
     title: "React Pinpoint",
     technologies: ["React", "Puppeteer", "Node js", "Docker", "Github Actions"],
     description: "An open-source utility library for measuring React component render times. Used in conjunction with Puppeteer js to test render times against a specified threshold. Also provides a CLI to generate Docker files for performance testing without influence from hardware limitations.",
-    demoLink: "https://reactpinpoint.com",
+    // demoLink: "https://reactpinpoint.com",
     sourceLink: "https://github.com/oslabs-beta/react-pinpoint",
   },
   {
     title: "Subway Time",
     technologies: ["HTML5", "CSS", "Express", "mta-gtfs"],
     description: "Frustrated by Google Map's inaccurate subway arrival and departure time reporting, I set out to create my own app to pull from the MTA's real-time data feeds. Users can search for and pull live data from any of the New York City's subway stations. The 'home' station is stored via the browser's local storage.",
-    demoLink: "https://subway-time.herokuapp.com",
+    demoLink: "https://subway-time.adaptable.app/",
     sourceLink: "https://github.com/jeffreyclu/subway-time",
   },
   {
     title: "Periodic Table of Sports",
     technologies: ["React with Hooks", "CSS", "Google Docs API"],
     description: "For this project, I collaborated with the Sports team at Perkins Eastman who wanted to display sports in a 'periodic table'. After several rounds of ideation, we settled on an interactive UI that lets users sort the table by different categories and filter the cards by different metrics. The database is a Google Sheet, so the Sports team can adjust the data and see changes reflected in real time.",
-    demoLink: "https://jstable.herokuapp.com",
+    demoLink: "https://jeffreyclu.github.io/periodic-table-of-sports/",
     sourceLink: "https://github.com/jeffreyclu/periodic-table-of-sports",
+  },
+  {
+    title: "Revit Journal Parser", 
+    technologies: ["Python", "Revit API", "TKinter"],
+    description: "A python based GUI application that parses Revit journal files and extracts useful information and statistics into a human readable format. Can be used to connect remotely to a user's computer (with admin permissions).",
+    sourceLink: "https://github.com/jeffreyclu/revit-journal-parser"
   },
   {
     title: "My Celiac Adventure",
     technologies: ["HTML5", "CSS", "React"],
     description: "My loving wife Janet was diagnosed with Celiac's disease a couple years ago. Inspired by her invariably frustrating experiences trying to eat out at restaurants, I created this educational game to simulate how every meal decision for people with Celiac's is a potentially life-threatening situation.",
-    demoLink: "https://my-celiac-adventure.herokuapp.com",
+    demoLink: "https://jeffreyclu.github.io/my-celiac-adventure/",
     sourceLink: "https://github.com/jeffreyclu/my-celiac-adventure",
   },
 ]
